24017a6844e6ac62ff9cd0553e17bdf01959842c
stony <stonyphoenix@gmail.com>
2023-01-04 19:10:20 -0500

    reto8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RetoModule } from './reto/reto.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RetoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
